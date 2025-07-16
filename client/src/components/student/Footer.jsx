aa795d6e2842283d99557557ed57afabbddffe62
Tejaswi kumar <brockthebone1389@gmail.com>
2025-07-16 12:51:05 +0530

    final commit

--- client/src/components/student/Footer.jsx
+++ client/src/components/student/Footer.jsx
@@ -9,7 +9,7 @@ const Footer = () => {
         <div className="flex flex-col md:items-start items-center w-full">
           <img src={assets.logo_dark} alt="logo" />
           <p className="mt-6 text-center md:text-left text-sm text-white/80">
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text.
+            Empowering learners and educators with seamless access to quality content, secure learning, and modern tools — all in one platform.
           </p>
         </div>
 
